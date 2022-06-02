f91482bdf9264522c27abaf221cc594ce446bcbc
MacLier <75802094+MacLier@users.noreply.github.com>
2022-06-02 15:14:34 +0200

    feat: add navbar recipe model

---
+++ 003/src/app/recipes/recipe.model.ts
@@ -0,0 +1,10 @@
+export class Recipe {
+    name: string;
+    description: string;
+    imagePath: string;
+    constructor(name: string, desc: string, iamgePath: string) {
+        this.name = name;
+        this.description = desc;
+        this.imagePath = this.imagePath;
+    }
+}
