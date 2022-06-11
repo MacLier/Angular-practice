7479969919b3b59aba8564a7530119fa1b10e383
MacLier <75802094+MacLier@users.noreply.github.com>
2022-06-11 14:38:44 +0200

    feat: shopping list & recipe list components setted

--- 003/src/app/recipes/recipe.model.ts
+++ 003/src/app/recipes/recipe.model.ts
@@ -2,9 +2,9 @@ export class Recipe {
     name: string;
     description: string;
     imagePath: string;
-    constructor(name: string, desc: string, iamgePath: string) {
+    constructor(name: string, desc: string, imagePath: string) {
         this.name = name;
         this.description = desc;
-        this.imagePath = this.imagePath;
+        this.imagePath = imagePath;
     }
 }
--- 003/src/app/shopping-list/shopping-list.component.ts
+++ 003/src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../models/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
+  public ingredients: Ingredient[] = [
+    new Ingredient('Apples', 5),
+    new Ingredient('Banana', 3)
+  ];
 
   constructor() { }
 
